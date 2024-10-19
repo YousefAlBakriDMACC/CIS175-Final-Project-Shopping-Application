106bcb47b26a07330ed62bf1a8484e9a58603935
ThuOfNumenor <josephlapras@gmail.com>
2024-10-19 00:06:23 -0500

    Initial Commit

---
+++ src/main/java/edu/dmacc/cis175/module15/tomcat10/cis175_final_project/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package edu.dmacc.cis175.module15.tomcat10.cis175_final_project;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/edu/dmacc/cis175/module15/tomcat10/cis175_final_project/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package edu.dmacc.cis175.module15.tomcat10.cis175_final_project.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
